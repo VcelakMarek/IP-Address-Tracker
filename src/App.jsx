23f652fd869c435372e714aa605f9224f9ff0a2e
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-30 01:33:43 +0100

    Update App.jsx
    add coordinatesContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import IpAddressTracker from "./IpAddressTracker";
+import coordinatesContext from "./coordinates.context";
 
 const App = () => {
-  return <IpAddressTracker />;
+  const coordinates = useState(coordinatesContext);
+  return (
+    <>
+      <coordinatesContext.Provider value={coordinates}>
+        <IpAddressTracker />;
+      </coordinatesContext.Provider>
+      ;
+    </>
+  );
 };
 
 const container = document.getElementById("root");
