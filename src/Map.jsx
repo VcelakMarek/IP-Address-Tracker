7fbf9efcf7e781dbf401124a64905517a6d008f6
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-30 01:34:26 +0100

    Update Map.jsx

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,28 @@
+import { useContext, useEffect, useState } from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import coordinatesContext from "./coordinates.context";
+import markerIcon from "./assets/icon-location.svg";
 
 const Map = () => {
+  const [coordinates] = useContext(coordinatesContext);
+  useEffect(() => {
+    var map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    var myIcon = new L.Icon({
+      iconUrl: markerIcon,
+      iconRetinaUrl: markerIcon,
+      popupAnchor: [-0, -0],
+      iconSize: [45, 55],
+    });
+    var marker = L.marker([51.5, -0.09], { icon: myIcon }).addTo(map);
+  }, []);
+
   return (
     <>
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
