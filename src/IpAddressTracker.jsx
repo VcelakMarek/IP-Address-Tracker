6432ca1fa799f9784e9cdbe084052b9c460a2a43
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-30 01:34:06 +0100

    Update IpAddressTracker.jsx

--- src/IpAddressTracker.jsx
+++ src/IpAddressTracker.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect, useContext } from "react";
 import Map from "./Map";
+import coordinatesContext from "./coordinates.context";
+import fetchIPify from "./fetchIPify";
 import "./scss/IpAddressTracker-styles.scss";
 
 const IpAddressTracker = () => {
+  const [ipAddress, setIpAddress] = useState("");
+  const [geolocation, setGeolocation] = useState("");
+  const [, setCoordinates] = useContext(coordinatesContext);
+  //   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+  //   console.log(API_KEY);
+  useEffect(() => {
+    fetchIPify("151.101.2.133", setGeolocation, setCoordinates);
+  }, []);
   return (
     <>
       <header>
@@ -11,36 +22,60 @@ const IpAddressTracker = () => {
             <input
               type="text"
               id="IpAddress"
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value)}
+              onKeyUp={(e) => {
+                if (e.key === "Enter")
+                  fetchIPify(ipAddress, setGeolocation, setCoordinates);
+              }}
               placeholder="Search for any IP address or domain"
             />
           </label>
-          <button>
+          <button
+            onClick={(e) => {
+              fetchIPify(ipAddress, setGeolocation, setCoordinates),
+                e.preventDefault();
+            }}
+          >
             <img src="./assets/icon-arrow.svg" alt="icon-arrow" />
           </button>
         </div>
         <section className="Ip-info">
           <ul>
             <li>
+              <div></div>
               <h3>IP ADDRESS</h3>
-              <h2>192.212.174.101</h2>
+              <h2> {geolocation != "" ? geolocation.ip : "not loaded yet"}</h2>
             </li>
             <li>
+              <div></div>
               <h3>LOCATION</h3>
-              <h2>Brooklyn, NY 10001</h2>
+              <h2>
+                {geolocation != ""
+                  ? `${geolocation.location.city}, ${geolocation.location.region}
+                ${geolocation.location.postalCode}`
+                  : "not loaded yet"}
+              </h2>
             </li>
             <li>
+              <div></div>
               <h3>TIMEZONE</h3>
-              <h2>Brooklyn, NY 10001</h2>
+              <h2>
+                {geolocation != ""
+                  ? geolocation.location.timezone
+                  : "not loaded yet"}
+              </h2>
             </li>
             <li>
               <h3>ISP</h3>
-              <h2>SpaceX Starlink</h2>
+              <h2>{geolocation != "" ? geolocation.isp : "not loaded yet"}</h2>
             </li>
           </ul>
         </section>
       </header>
-
-      <Map id="map" />
+      <div id="map">
+        <Map />
+      </div>
     </>
   );
 };
